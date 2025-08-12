2bed4b71a54d71fc4ddbe6353dfeb0d6c3e29dfd
JoaquinLagares <joacoglagares@gmail.com>
2025-08-12 09:24:02 -0300

    update main for more clarity

--- main.js
+++ main.js
@@ -1,69 +1,40 @@
 let computerScore = 0;
 let userScore = 0;
-let roundCount = 0;
+let roundCount = 1;
+const player = document.querySelector('#player')
+const playerPoints = document.querySelector('#player-points')
+const computer = document.querySelector('#computer')
+const computerPoints = document.querySelector('#computer-points')
+const pResult = document.querySelector('#result')
 const options = document.querySelector('#options')
 
-let getComputerChoice = () => {
-    let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice == 1) {
-        return "ROCK"
-    } else if (computerChoice == 2) {
-        return "PAPER"
-    } else {
-        return "SCISSORS"
-    }
+playerPoints.textContent = `Player points: ${userScore}`;
+computerPoints.textContent = `Computer points: ${computerScore}`;
+let picks = ['ROCK', 'PAPER', 'SCISSORS']
 
+const victorias = {
+    ROCK: 'SCISSORS',
+    PAPER: 'ROCK',
+    SCISSORS: 'PAPER'
 }
-let getUserChoice = () => {
-    options.addEventListener('click', (event) => {
-        let target = event.target
-        switch (target.id) {
-            case 'rock':
-                console.log('rock')
-                return 'ROCK'
-            case 'paper':
-                console.log('paper')
-                return 'PAPER'
-            case 'scissors':
-                console.log("scissors")
-                return 'SCISSORS'
-        }
-    })
+
+let getComputerChoice = () => {
+    return picks[Math.floor(Math.random() * picks.length)]
 }
 
 let comparator = (computerChoice, userChoice) => {
-    let result;
-    if (computerChoice === userChoice) {
-        return "it's a draw"
+    if (computerChoice === userChoice) return `it's a draw`
+    else if (victorias[computerChoice] === userChoice) {
+        computerScore++;
+        return 'computer wins';
     }
-    else if (computerChoice === 'ROCK') {
-        if (userChoice === 'SCISSORS') {
-            return 'Computer wins';
-        } else if (userChoice == 'PAPER') {
-            return 'User wins'
-        }
+    else {
+        userScore++;
+        return 'user wins';
     }
-    else if (computerChoice === 'PAPER') {
-        if (userChoice === 'ROCK') {
-            result = "Computer wins";
-        } else if (userChoice == "SCISSORS") {
-            return 'User wins'
-        }
-    }
-    else if (computerChoice === 'SCISSORS') {
-        if (userChoice === 'PAPER') {
-            return 'Computer wins';
-        } else if (userChoice == "ROCK") {
-            return 'User wins'
-        }
-    }
-
-
-
 }
 
 let playRound = () => {
-
 
     options.addEventListener('click', (event) => {
         roundCount++
@@ -82,52 +53,27 @@ let playRound = () => {
                 break
         }
         let computerChoice = getComputerChoice()
-        midRound(computerChoice, userChoice)
-        if (userScore === 5 || computerScore === 5)
-            endRound(computerChoice, userChoice);
+        pResult.textContent = `This round ${comparator(computerChoice, userChoice)}`
+
+        playerPoints.textContent = `Player points: ${userScore}`;
+        computerPoints.textContent = `Computer points: ${computerScore}`;
+        if (userScore === 5 || computerScore === 5) {
+            playerPoints.textContent = `Player points: ${userScore}`;
+            computerPoints.textContent = `Computer points: ${computerScore}`;
+            setTimeout(() => {
+                alert(`Game finished ${userScore === 5 ? "player wins" : "computer wins"}`);
+                userScore = 0;
+                computerScore = 0;
+                playerPoints.textContent = `Player points: ${userScore}`;
+                computerPoints.textContent = `Computer points: ${computerScore}`;
+            }, 50)
+        }
+
     })
 
+
+
+
 }
 
-
-let midRound = (computerChoice, userChoice) => {
-
-    const div = document.querySelector('#history')
-    const pRoundCount = document.createElement('p')
-    pRoundCount.textContent = `-----RoundCount: ${roundCount}-----`
-    div.appendChild(pRoundCount)
-    const pUserChoice = document.createElement('p')
-    pUserChoice.textContent = `User choice: ${userChoice}`
-    div.appendChild(pUserChoice)
-    const pComputerChoice = document.createElement('p')
-    pComputerChoice.textContent = `Computer choice: ${computerChoice}`
-    document.querySelector('#history').appendChild(pComputerChoice)
-    console.log(`user choice: ${userChoice}`)
-    let comparison = comparator(computerChoice, userChoice)
-    if (comparison === "it's a draw")
-        console.log("it's a draw")
-    else if (comparison === 'User wins')
-        userScore++
-    else if (comparison === 'Computer wins')
-        computerScore++
-
-    const pUserScore = document.createElement('p')
-    pUserScore.textContent = `User score: ${userScore}`
-    div.appendChild(pUserScore)
-    const pComputerScore = document.createElement('p')
-    pComputerScore.textContent = `Computer score: ${computerScore}`
-    div.appendChild(pComputerScore)
-}
-let endRound = () => {
-
-    const div = document.querySelector('#history')
-    const result = document.createElement('h1')
-    result.textContent = `The winner is: ${computerScore === 5 ? "computer" : "user"}`
-    div.appendChild(result)
-    userScore = 0;
-    computerScore = 0;
-}
-
-
-playRound();
-
+playRound()
